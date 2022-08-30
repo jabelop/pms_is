c082bcb396a614432725395092a1d471b217c6b1
jabelop <jabelop@hotmail.com>
2022-08-30 15:20:37 -0400

    - model generator package added to app providers

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // model generator provider
+        Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
+
     ],
 
     /*
