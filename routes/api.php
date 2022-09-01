93b1226ee6ddc71762e8d9a48fb3a5b8db57d8cf
jabelop <jabelop@hotmail.com>
2022-09-01 08:48:22 -0400

    - application routes implemented

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// user routes
+Route::post('user', 'App\Http\Controllers\UserController@postUser');
+Route::get('users', 'App\Http\Controllers\UserController@getUsers');
+
+// project routes
+Route::post('project', 'App\Http\Controllers\ProjectController@postProject');
+Route::put('project-user', 'App\Http\Controllers\ProjectController@addUser');
+Route::get('projects', 'App\Http\Controllers\ProjectController@getProjects');
+
+// activity routes
+Route::post('activity', 'App\Http\Controllers\ActivityController@postActivity');
+Route::put('activity-user', 'App\Http\Controllers\ActivityController@addUser');
+Route::get('activities', 'App\Http\Controllers\ActivityController@getActivities');
+
+// incidence routes
+Route::post('incidence', 'App\Http\Controllers\IncidenceController@postIncidence');
+Route::get('incidences', 'App\Http\Controllers\IncidenceController@getIncidences');
+
+
